c8d893a33d238b50b3ac36dbf85738c8fb37dc72
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-08 12:48:59 +0200

    pokemon agua

---
+++ pokemon_agua.py
@@ -0,0 +1,132 @@
+from pokemon import pokemon
+from weapon_type import WeaponType
+
+
+class PokemonWater(pokemon):
+    
+
+
+    def __init__(self,id,nombre,arma,salud,ataque,defensa):
+        
+
+        super().__init__(id,nombre,arma,salud,ataque,
+                         10, defensa)
+
+        if isinstance(ataque, int):
+            if 11 <= ataque <= 20:
+                self._ataque = ataque
+            else:
+                raise ValueError("The parameter attack_rating should be > 11 and <= 20.")
+        else:
+            raise TypeError("The parameter attack_rating should be a int.")
+
+
+    def set_ataque(self, ataque_to_be_set):
+        
+        if isinstance(ataque_to_be_set, int):
+            if 11 <= ataque_to_be_set <= 20:
+                self._ataque = ataque_to_be_set
+            else:
+                raise ValueError("The parameter attack_rating_to_be_set should be > 11 and <= 20.")
+        else:
+            raise TypeError("The parameter attack_rating_to_be_set should be a int.")
+
+
+def main():
+    
+
+    print("=================================================================.")
+    print("Test Case 1: Create a Pokemon.")
+    print("=================================================================.")
+    pokemon_1 = PokemonWater(1, "Squirtle", WeaponType.HEADBUTT, 100, 12, 8)
+
+    if pokemon_1.get_nombre() == "Squirtle":
+        print("Test PASS. The parameter pokemon_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_arma().name == "HEADBUTT":
+        print("Test PASS. The parameter weapon_type has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_salud() == 100:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_ataque() == 12:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_defensa() == 8:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+
+    print("=================================================================.")
+    print("Test Case 2: Human-readable format of the object.")
+    print("=================================================================.")
+    pokemon_2 = PokemonWater(7, "Squirtle", WeaponType.HEADBUTT, 100,15, 7)
+
+    if str(pokemon_2) == "Pokemon ID 7 with name Squirtle has as weapon HEADBUTT and health 100":
+        print("Test PASS. The human-readable format of the object has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
+
+
+    print("=================================================================.")
+    print("Test Case 3: Pokemon alive?¿?.")
+    print("=================================================================.")
+    pokemon_3 = PokemonWater(3, "Squirtle", WeaponType.KICK, 97, 15, 8)
+
+    if pokemon_3.is_alive():
+        pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
+
+        if not pokemon_3.is_alive():
+            print("Test PASS. The method is_alive() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method is_alive().")
+    else:
+        print("Test FAIL. Check the method is_alive().")
+
+
+    print("=================================================================.")
+    print("Test Case 4: Check the defense during a Fight.")
+    print("=================================================================.")
+    pokemon_4 = PokemonWater(4, "Squirtle", WeaponType.ELBOW, 93, 11, 9)
+
+    pokemon_4.fight_defense(70)
+
+    if pokemon_4.get_health_points() == 32:
+        print("Test PASS. The method fight_defense() has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method fight_defense().")
+
+
+    print("=================================================================.")
+    print("Test Case 5: Check the attack during a Fight.")
+    print("=================================================================.")
+    pokemon_5 = PokemonWater(5, "Squirtle", WeaponType.PUNCH, 99, 20, 10)
+    pokemon_6 = PokemonWater(6, "Squirtle", WeaponType.PUNCH, 99, 18, 9)
+
+    pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
+
+    if pokemon_was_hit:
+        if pokemon_6.get_salud() == 88:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+    else:
+        if pokemon_6.get_salud() == 99:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+
+
+
+# Checking whether this module is executed just itself alone.
+if __name__ == "__main__":
+    main()
